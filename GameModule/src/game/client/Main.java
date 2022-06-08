d76540ec6c39b6fea05a1a3450ab8e74f39a8bf6
Sachin Gurung <saching061@hotmail.com>
2022-06-08 14:23:02 -0700

    Added Main method and Game method which shows the ASCII art and welcome message with background text colors AB#151

---
+++ GameModule/src/game/client/Main.java
@@ -0,0 +1,11 @@
+package game.client;
+
+
+import game.Game;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Game.asciiArt();
+        Game.welcome();
+    }
+}
