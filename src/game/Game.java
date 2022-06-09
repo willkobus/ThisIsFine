1cf2a98de2c04dbafdc85c8516e791e4a92fbdd6
Phil Schmeiser <pschmeis@amazon.com>
2022-06-09 14:35:18 -0500

    added quit, fixed loop and AB#152

--- src/game/Game.java
+++ src/game/Game.java
@@ -7,11 +7,33 @@ import static game.ColorEnums.*;
 
 public class Game {
 
-    public static void execute() throws InterruptedException {
+    Player player = new Player();
+    ActionParser actionParser = new ActionParser();
+
+    public void execute() throws Exception {
         asciiArt();
         welcome();
+
+//        while(start());
+        boolean quit = false;
+
+        while(!quit) {
+            quit = actionParser.playerMove(player);
+        }
+
+
     }
 
+    private boolean start() throws Exception {
+        return !actionParser.playerMove(player);
+
+        /*
+        while !quit
+            quit = actionparser.playermove(player)
+
+         */
+
+    }
 
     // ASCII Art method
     public static void asciiArt() {
@@ -47,19 +69,20 @@ public class Game {
 
     // Welcome method
     public static void welcome() throws InterruptedException {
+        int delay = 500;
         System.out.println();
         System.out.println("Hello VOLUNTEER TESTER #18978983! We are grateful that you have signed up for this trial. We also are grateful that you didn’t ask any questions ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println("as to what the trial would be and still signed the waiver! Safety is our number one concern here at Practical Applications Inc. Now that the door ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println("has locked behind you in this random house we had you come to, you may be wondering what it is we are going to have you do. Well simply put, we ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println("are conducting a study to see how well people solve problems when faced with a stressful situation. In your case it seems you are in situation #4352, ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println(RED_TEXT + "“Escape the Burning Building”" + RESET_TEXT + ". In this scenario you have to solve puzzles and complete tasks in order to get all the keys and escape the building ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println("in a set number of moves. How many moves you ask? Well, to keep you good and stressed, you don’t get to know that! Don’t worry though, we will give ");
-        Thread.sleep(2000);
+        Thread.sleep(delay);
         System.out.println("periodic updates to let you know how you are doing! Good luck!");
     }
 
