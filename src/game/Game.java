37e6064bd8e9865cfe09adb8c0234bee9e02d205
Sachin Gurung <saching061@hotmail.com>
2022-06-13 14:28:06 -0700

    Added Ascii art for win and lose condition. Added gameText.json file and working on reading from it for welcome screen. AB#405 AB#161

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,5 @@
 package game;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-
 import static game.ColorEnums.*;
 
 public class Game {
@@ -11,7 +8,7 @@ public class Game {
     ActionParser actionParser = new ActionParser();
 
     public void execute() throws Exception {
-        asciiArt();
+        AsciiArts.asciiArtThisIsFine();
         welcome();
         gameRules();
 
@@ -43,41 +40,17 @@ public class Game {
         System.out.println("===============================================================================================");
     }
 
-    // ASCII Art method
-    public static void asciiArt() {
-        int width = 100;
-        int height = 30;
-
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 15));
-
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("THIS IS FINE", 5, 20);
-
-
-        for (int y = 0; y < height; y++) {
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < width; x++) {
-
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : "&");
-
-            }
-
-            if (sb.toString().trim().isEmpty()) {
-                continue;
-            }
-
-            System.out.println(GREEN_TEXT + sb + RESET_TEXT);
-        }
-
-    }
-
     // Welcome method
     public static void welcome() throws InterruptedException {
         int delay = 500;
+//        String strJson = getJSONFromFile("resources/gameText.json");
+//
+//        System.out.println(strJson);
+//        for (int i=0; i<strJson.length(); i++){
+//            JSONObject strJsonLine = (JSONObject) strJson.(i);
+//            System.out.println(strJsonLine);
+//        }
+
         System.out.println();
         System.out.println("Hello VOLUNTEER TESTER #18978983! We are grateful that you have signed up for this trial. We also are grateful that you didn't ask any questions ");
         Thread.sleep(delay);
@@ -85,11 +58,11 @@ public class Game {
         Thread.sleep(delay);
         System.out.println("has locked behind you in this random house we had you come to, you may be wondering what it is we are going to have you do. Well simply put, we ");
         Thread.sleep(delay);
-        System.out.println("are conducting a study to see how well people solve problems when faced with a stressful situation. In your case it seems you are in situation #4352, ");
+        System.out.println("are conducting a study to see how well people solve problems when faced with a stressful situation. In your case it seems you are in situation ");
         Thread.sleep(delay);
-        System.out.println(RED_TEXT + "'Escape the Burning Building'" + RESET_TEXT + ". In this scenario you have to solve puzzles and complete tasks in order to get all the keys and escape the building ");
+        System.out.println("#4352, " + RED_TEXT + "'Escape the Burning Building'" + RESET_TEXT + ". In this scenario you have to solve puzzles and complete tasks in order to get all the keys and escape the ");
         Thread.sleep(delay);
-        System.out.println("in a set number of moves. How many moves you ask? Well, to keep you good and stressed, you don't get to know that! Don’t worry though, we will give ");
+        System.out.println("building in a set number of moves. How many moves you ask? Well, to keep you good and stressed, you don't get to know that! Don’t worry though, we will give ");
         Thread.sleep(delay);
         System.out.println("periodic updates to let you know how you are doing! Good luck!");
     }
--- src/game/JSONRead.java
+++ src/game/JSONRead.java
@@ -1,13 +1,10 @@
 package game;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class JSONRead {
     public JSONObject readJSON() throws Exception{
@@ -18,6 +15,39 @@ public class JSONRead {
 
         return jo;
     }
+
+//    public class readJSONFromWelcome() {
+//        Object obj = new JSONParser().parse(new FileReader("resources/gameText.json"));
+//
+//        public readJSONFromWelcome() throws IOException, ParseException {
+//        }
+//    }
+//     read from gameText.json
+    public static String getJSONFromFile(String filename){
+        String jsonTextFromWelcome = "";
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                jsonTextFromWelcome+=line+"\n";
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonTextFromWelcome;
+    }
+
+
+
+
+//    public JSONObject readGameText() throws Exception {
+//        Object obj = new JSONParser().parse(new FileReader("resources/gameText.json"));
+//
+//        // typecasting obj to JSONObject
+//        JSONObject joGameText = (JSONObject) obj;
+//        return joGameText;
+//    }
 }
 
 
