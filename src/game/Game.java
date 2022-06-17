9e4dae24821ad29c5943cf4759014752fde79170
Sachin Gurung <saching061@hotmail.com>
2022-06-17 14:52:32 -0700

    removed pause between door close and fire sound AB#181

--- src/game/Game.java
+++ src/game/Game.java
@@ -33,11 +33,6 @@ public class Game {
     // Welcome method
     public static void welcome() {
         MusicPlayer.playDoorCloseMusic();
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         Scanner scan = new Scanner(System.in);
         AsciiArts.asciiArtThisIsFine();
